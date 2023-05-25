cae63b5297a1a5292fe7a173d13a8b4405474ec1
dewikhulbi <dewikhlb20@gmail.com>
2023-05-25 15:31:01 +0700

    START berisi NULL atau nilai kosong

--- Doublelinkedlisist_132/Doublelinkedlisist_132.cpp
+++ Doublelinkedlisist_132/Doublelinkedlisist_132.cpp
@@ -16,4 +16,15 @@ private:
 public:
 	DoubleLinkedList();
 	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
 };
+
+DoubleLinkedList::DoubleLinkedList() {
+	START = NULL;
+}
